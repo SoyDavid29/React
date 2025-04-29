1b88a5afc97705a630be2c9092cf73f1e6b5cdaa
DavidRodriguez <studiandoprogramacion@gmail.com>
2025-04-29 15:21:29 -0600

    slight change done on the useState button

---
+++ src/Menu.jsx
@@ -0,0 +1,9 @@
+export default function Menu({open}){
+    return(
+        <div className={`main-menu ${open ? "show" : ""}`}>
+            <p>Firts Link</p>
+            <p>Second Link</p>
+            <p>Thrid Link</p>
+        </div>
+    )
+}
